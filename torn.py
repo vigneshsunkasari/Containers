614aac8fb327d5fcf55f91f447e7a72125d48c14
vigneshsunkasari <79750378+vigneshsunkasari@users.noreply.github.com>
2024-06-05 16:18:39 +0000

    Initial commit msg

--- torn.py
+++ torn.py
@@ -1,7 +1,15 @@
+# import flask module
 from flask import Flask
-app=Flask(__name__)
-@app.route('/home')
-def helloworld():
-    return "HelloWorld!!"
-if __name__=="__main__":
-    app.run(port=5000,debug=True)
+
+# instance of flask application
+app = Flask(__name__)
+
+
+# home route that returns below text 
+# when root url is accessed
+@app.route("/")
+def hello_world():
+	return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+	app.run(debug=True, port=8080)
